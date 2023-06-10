ec24b9919af63fec3e56c5c3efbfd318b9e92704
khoi.le <khoi.le@advahealthsolutions.com>
2023-06-10 10:37:58 +0700

    add parts components

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import LogoLetter from "./assets/images/logo-letter.png";
+import LogoLetterWhite from "./assets/images/logo-letter-white.png";
+import LogoDarkText from "./assets/images/logo-dark-text.png";
+import LogoLightText from "./assets/images/logo-light-text.png";
+
+export { LogoDarkText, LogoLetter, LogoLetterWhite, LogoLightText };
