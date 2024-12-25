c8b025c2fe1ba32b8854353d5cb9d9769b0a7494
LÊ MINH KHÔI <leminhkhoi@Minh-Chau.local>
2024-12-25 13:53:01 +0700

    remove title

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,19 +1,3 @@
-/*
- * AdvaHealth Solutions Pty. Ltd. ("AHS") CONFIDENTIAL
- * Copyright (c) 2022 AdvaHealth Solutions Pty. Ltd. All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains the property of AHS. The intellectual and technical concepts contained
- * herein are proprietary to AHS and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
- * from AHS.  Access to the source code contained herein is hereby forbidden to anyone except current AHS employees, managers or contractors who have executed
- * Confidentiality and Non-disclosure agreements explicitly covering such access.
- *
- * The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code, which includes
- * information that is confidential and/or proprietary, and is a trade secret, of AHS. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
- * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
- * LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
- * TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
- */
 
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { FC } from "react";
@@ -124,9 +108,8 @@ export const FormModal: FC<FormModalProps> = (props: FormModalProps) => {
             key={new Date().getMilliseconds() + Math.random()}
             type="button"
             disabled={disabled}
-            className={`btn ${
-              confirmBtnVariant ? `btn-${confirmBtnVariant}` : "primary"
-            } ml-2`}
+            className={`btn ${confirmBtnVariant ? `btn-${confirmBtnVariant}` : "primary"
+              } ml-2`}
             onClick={(e) => {
               confirm(e);
             }}
--- src/components/Table/TableFooter.tsx
+++ src/components/Table/TableFooter.tsx
@@ -1,19 +1,3 @@
-/*
- * AdvaHealth Solutions Pty. Ltd. ("AHS") CONFIDENTIAL
- * Copyright (c) 2022 AdvaHealth Solutions Pty. Ltd. All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains the property of AHS. The intellectual and technical concepts contained
- * herein are proprietary to AHS and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
- * from AHS.  Access to the source code contained herein is hereby forbidden to anyone except current AHS employees, managers or contractors who have executed
- * Confidentiality and Non-disclosure agreements explicitly covering such access.
- *
- * The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code, which includes
- * information that is confidential and/or proprietary, and is a trade secret, of AHS. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
- * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
- * LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
- * TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
- */
 
 import React, { FC, useCallback, useEffect, useState } from "react";
 import { SearchPaginateType } from "../../types/generic";
@@ -78,18 +62,15 @@ export const TableFooter: FC<FooterData> = ({
       <div className="dataTables_paginate paging_simple_numbers d-flex align-items-center">
         <span>
           {totalElements
-            ? `Showing ${totalElements > 0 ? offset + 1 : 0} to ${
-                offset + numberOfElements || 0
-              } of ${totalElements}`
-            : `Showing ${numberOfElements > 0 ? offset + 1 : 0} to ${
-                offset + numberOfElements || 0
-              }`}
+            ? `Showing ${totalElements > 0 ? offset + 1 : 0} to ${offset + numberOfElements || 0
+            } of ${totalElements}`
+            : `Showing ${numberOfElements > 0 ? offset + 1 : 0} to ${offset + numberOfElements || 0
+            }`}
         </span>
         <ul className="pagination mb-0 pb-0 ml-2">
           <li
-            className={`paginate_button page-item previous cursor-pointer ${
-              first ? "disabled" : ""
-            }`}
+            className={`paginate_button page-item previous cursor-pointer ${first ? "disabled" : ""
+              }`}
           >
             <a
               type="button"
@@ -100,9 +81,8 @@ export const TableFooter: FC<FooterData> = ({
             </a>
           </li>
           <li
-            className={`paginate_button page-item next cursor-pointer ${
-              last ? "disabled" : ""
-            }`}
+            className={`paginate_button page-item next cursor-pointer ${last ? "disabled" : ""
+              }`}
           >
             <a
               type="button"
